a2bf34f19e605acbf094bd8bf4437dc0f9791c74
mvp134 <35764307+mvp134@users.noreply.github.com>
2026-05-25 09:30:26 +0900

    Update changelog.js

--- changelog.js
+++ changelog.js
@@ -1,6 +1,11 @@
 // changelog.js
 const CHANGELOG_DATA = [
   {
+    version: "v1.0.1",
+    date: "2026.05.25",
+    desc: "- 모바일 보기 최적화"
+  },
+  {
     version: "v1.0.0",
     date: "2026.05.24",
     desc: "- 도화첩 최적화 알파 버전 배포<br>- UI 타이틀 하단에 변경기록 및 의견 접수 버튼 추가"
